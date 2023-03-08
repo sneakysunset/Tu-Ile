d09067660a74a6d04a5836f6a35c975cf37dc688
VincentLeVrai <107613765+VincentLeVrai@users.noreply.github.com>
2023-03-08 13:51:35 +0100

    Avatar Regarde dans la direction où il se dirige - 08/03

--- Assets/Scripts/Bateden/MouvemenntDePersonnage.cs
+++ Assets/Scripts/Bateden/MouvemenntDePersonnage.cs
@@ -7,9 +7,10 @@ public class MouvemenntDePersonnage : MonoBehaviour
 {
     private CharacterController controller;
     public float speed = 5f;
-    public Vector2 moveValue;
+    Vector2 moveValue;
     [Range(-10f,10f)] 
-    public float laForceEstEnMoi = 1f;
+    public float forceDeGravite = 1f;
+    Vector3 move;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -24,10 +25,11 @@ public class MouvemenntDePersonnage : MonoBehaviour
         }
         else
         {
-            move.y -= 9.81f * laForceEstEnMoi * Time.deltaTime;
+            move.y -= 9.81f * forceDeGravite * Time.deltaTime;
         }
         controller.Move(move);
-        
+        if (moveValue.x !=0 || moveValue.y !=0)
+            transform.forward = new Vector3(moveValue.x, 0, moveValue.y);
     }
     public void OnDeplacement(InputAction.CallbackContext cbx)
     {
