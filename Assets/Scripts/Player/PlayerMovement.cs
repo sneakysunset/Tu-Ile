b0fd471aa360c0481bbec10bfb9c6c6288ccc4a6
sneakysunset <andrelauzier@gmail.com>
2023-03-14 17:05:27 +0100

    Scripts Icons - 14 03 23

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,6 +14,13 @@ public class PlayerMovement : MonoBehaviour
     [HideInInspector] public EventInstance movingSound;
 
     [SerializeField] private float speed;
+    [SerializeField] private float sprintingSpeed;
+    [Header("1 = 0.1 sec, .1 = 1 sec")]
+    [SerializeField, Range(0.01f, 1)] private float acceleration = .7f;
+    [SerializeField, Range(0.01f, 1)] private float deceleration = .6f;
+    [Space(10)]
+    private float speedValue;
+    bool isSprinting;
     bool moveFlag;
     #endregion
     #region Variables: Rotation
@@ -33,6 +40,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         movingSound = FMODUnity.RuntimeManager.CreateInstance("event:/Tile/Charactere/moov");
+        movingSound.set3DAttributes(new FMOD.ATTRIBUTES_3D());
         _characterController = GetComponent<CharacterController>();
     }
 
@@ -51,6 +59,7 @@ public class PlayerMovement : MonoBehaviour
         groundedCallback = _characterController.isGrounded;
         ApplyGravity();
         ApplyRotation();
+        SpeedModifier();
         ApplyMovement();
         if(_characterController.isGrounded && _input != Vector2.zero && !moveFlag)
         {
@@ -63,6 +72,7 @@ public class PlayerMovement : MonoBehaviour
             movingSound.stop(STOP_MODE.ALLOWFADEOUT);
         }
     }
+
 
     private void ApplyGravity()
     {
@@ -87,9 +97,15 @@ public class PlayerMovement : MonoBehaviour
         transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
     }
 
+    private void SpeedModifier()
+    {
+        float ax = isSprinting ? acceleration : -deceleration;
+        speedValue = Mathf.Lerp(speed, sprintingSpeed, ax * Time.deltaTime * 10);
+    }
+
     private void ApplyMovement()
     {
-        _characterController.Move(_direction * speed * Time.deltaTime);
+        _characterController.Move(_direction * speedValue * Time.deltaTime);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -98,6 +114,17 @@ public class PlayerMovement : MonoBehaviour
         _direction = new Vector3(_input.x, 0.0f, _input.y);
     }
 
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isSprinting = true;
+        }
+        else if(context.canceled || context.performed)
+        {
+            isSprinting = false;
+        }
+    }
 
     private void OnDestroy()
     {
