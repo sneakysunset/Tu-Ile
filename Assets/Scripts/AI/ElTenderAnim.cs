3c46a1bc5d80592974bbabf08bcb84ac007b627a
Sneakysunset <andrelauzier@gmail.com>
2023-03-27 01:30:59 +0200

    Chicken Sounds - 27 03 23

--- Assets/Scripts/AI/ElTenderAnim.cs
+++ Assets/Scripts/AI/ElTenderAnim.cs
@@ -1,14 +1,25 @@
+using FMOD.Studio;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ElTenderAnim : MonoBehaviour
 {
+    
     Animator[] animators;
     AI_Movement AIM;
     Item_Bird itB;
+    bool isHeldChecker;
+    private FMOD.Studio.EventInstance fmodGrabbed;
+    private FMOD.Studio.EventInstance fmodIdle;
+
     private void Start()
     {
+        fmodGrabbed = FMODUnity.RuntimeManager.CreateInstance("event:/Monster/Angry_Chicken");
+        fmodIdle = FMODUnity.RuntimeManager.CreateInstance("event:/Monster/Idle_Chicken");
+        fmodIdle.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform));
+        fmodGrabbed.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform));
+        fmodIdle.start();
         animators = GetComponentsInChildren<Animator>();
         AIM = GetComponent<AI_Movement>();
         itB = GetComponent<Item_Bird>();
@@ -16,8 +27,27 @@ public class ElTenderAnim : MonoBehaviour
 
     private void Update()
     {
+        fmodIdle.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform));
+        fmodGrabbed.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform));
+        if (isHeldChecker && !itB.isHeld)
+        {
+            fmodIdle.start();
+            if (FMODUtils.IsPlaying(fmodGrabbed))
+            {
+                fmodGrabbed.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            }
+        }
+
         if(itB.isHeld)
         {
+            if (!isHeldChecker)
+            {
+                fmodGrabbed.start();
+                if (FMODUtils.IsPlaying(fmodIdle))
+                {
+                    fmodIdle.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                }
+            }
             foreach(Animator animator in animators) 
             {
                 animator.Play("Grabbed", 0);
@@ -37,5 +67,23 @@ public class ElTenderAnim : MonoBehaviour
                 animator.Play("Idle", 0);
             }
         }
+        isHeldChecker = itB.isHeld;
+
+        
+    }
+    private void OnDestroy()
+    {
+        if (FMODUtils.IsPlaying(fmodIdle))
+        {
+            fmodIdle.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        }
+        if (FMODUtils.IsPlaying(fmodGrabbed))
+        {
+            fmodGrabbed.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        }
+
+        fmodGrabbed.release();
+        fmodIdle.release();
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Monster/Dying_Chicken");
     }
 }
