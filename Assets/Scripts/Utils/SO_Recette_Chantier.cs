24cdd305187d50ecdd6283e2c90bcc56ada29a43
VincentLeVrai <107613765+VincentLeVrai@users.noreply.github.com>
2023-05-23 10:06:59 +0200

    Les entendé vous crier au secour

--- Assets/Scripts/Utils/SO_Recette_Chantier.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Scriptable Objects/Chantier", menuName = "Recettes/Chantier", order = 1)]
-public class SO_Recette_Chantier : SO_Recette
-{
-    public Item_Crate loot;
-}
